f79f0f31922609aced57bab798fc9b01a9b8398b
NairbSaiko <93670726+BrianPestano@users.noreply.github.com>
2024-01-14 16:55:22 +0100

    Limpieza de codigo, dia 1 comienza el trabajo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.proyectofinalfirebasebrianylauren"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.proyectofinalfirebasebrianylauren"
--- app/src/main/java/com/example/proyectofinalfirebasebrianylauren/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinalfirebasebrianylauren/MainActivity.kt
@@ -11,50 +11,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.composable
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.navArgument
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            proyectofinalfirebasebrianylauren {
+            ProyectoFinalFireBaseBrianyLaurenTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // Crea el NavController
-                    val navController = rememberNavController()
 
-                    // Configuración del sistema de navegación
-                    SetupNavigation(navController)
                 }
             }
         }
     }
 }
-@Composable
-fun SetupNavigation(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "pantallaPrincipal") {
-        composable("pantallaPrincipal") { pantallaPrincipal (navController) }
-        composable(
-            "pantallaDetalles/{pokemonNombre}",
-            arguments = listOf(navArgument("pokemonNombre") { type = NavType.StringType })
-        ) { backStackEntry ->
-            val pokemonNombre = backStackEntry.arguments?.getString("pokemonNombre")
-            pantallaDetalles(navController,pokemonNombre)
 
-        }
-        composable("CreacionVJ/{region}") { backStackEntry ->
-            val region = backStackEntry.arguments?.getString("region")
-            CreacionVJ(navController)
-        }
-    }
-}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -62,10 +36,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    proyectofinalfirebasebrianylauren {
+    ProyectoFinalFireBaseBrianyLaurenTheme {
         Greeting("Android")
     }
 }
