42665fa849cf769c6f1332447a6dd3baf374fcc8
NairbSaiko <93670726+BrianPestano@users.noreply.github.com>
2024-02-18 12:15:27 +0100

    dia 43 again, retoques minimos

--- app/src/main/java/com/example/proyectofinalfirebasebrianylauren/ui/theme/Color.kt
+++ app/src/main/java/com/example/proyectofinalfirebasebrianylauren/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package com.example.proyectofinalfirebasebrianylauren.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFFF02B3C)
+val PurpleGrey80 = Color(0xFF625b71)
+val Pink80 = Color(0xFFFFEB3B)
 
 val Purple40 = Color(0xFFF02B3C)
 val PurpleGrey40 = Color(0xFF625b71)
